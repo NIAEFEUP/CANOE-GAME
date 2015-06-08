7278527ec008a70c4bab591acf06e71c75955d7d
Sk0ut <poke_flavio@hotmail.com>
2015-06-08 11:14:56 +0100

    no idea

---
+++ GameEngine/core/src/Engine/GameScreen.java
@@ -0,0 +1,43 @@
+package Engine;
+
+import Server.Server;
+import com.badlogic.gdx.ApplicationAdapter;
+
+/**
+ * Created by Flávio on 07/06/2015.
+ */
+public class GameScreen extends ApplicationAdapter{
+    Track track;
+    GDXTrackRenderer renderer;
+    TrackController controller;
+
+    @Override
+    public void create() {
+        TrackConfig cfg = new TrackConfig();
+        cfg.width = 10f;
+        cfg.length = 30f;
+        cfg.margin = 5f;
+        track = new Track(cfg);
+        renderer = new GDXTrackRenderer(track);
+        Server server = new Server();
+        new Thread(server).start();
+
+        track.start();
+        track.getCanoe().getBody().setLinearVelocity(0f, 0f);
+
+        controller = new RemoteTrackController(track, server);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        renderer.resize(width, height);
+    }
+
+    @Override
+    public void render() {
+        controller.processInput();
+        track.update(1f / 60f);
+        renderer.render();
+    }
+}
--- GameEngine/core/src/Engine/LoginScreen.java
+++ GameEngine/core/src/Engine/LoginScreen.java
@@ -3,5 +3,5 @@ package Engine;
 /**
  * Created by Flávio on 08/06/2015.
  */
-public class GameEngine {
+public class LoginScreen {
 }
